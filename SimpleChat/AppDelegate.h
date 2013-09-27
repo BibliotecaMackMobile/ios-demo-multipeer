fc531c1e419d69ca29ba6de12301ccd2ce604e40
Tung Nguyen <tungnguyen@Mac09.local>
2013-09-27 16:42:05 +0700

    Initial Commit

---
+++ SimpleChat/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimpleChat
+//
+//  Created by Tung Nguyen on 9/27/13.
+//  Copyright (c) 2013 Tung Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SimpleChat/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SimpleChat
+//
+//  Created by Tung Nguyen on 9/27/13.
+//  Copyright (c) 2013 Tung Nguyen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
