576c9ae7c6e269d10130a086c055868a6ba71a9f
rogBordignon <rogeriobordignon@gmail.com>
2014-03-11 10:50:23 -0300

    first commit

--- SimpleChat/ViewController.h
+++ SimpleChat/ViewController.h
@@ -7,7 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MultipeerConnectivity/MultipeerConnectivity.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <MCBrowserViewControllerDelegate, MCSessionDelegate, UITextFieldDelegate>
+
+
+@property (nonatomic, strong) MCBrowserViewController *browserVC;
+@property (nonatomic, strong) MCAdvertiserAssistant *advertiser;
+@property (nonatomic, strong) MCSession *mySession;
+@property (nonatomic, strong) MCPeerID *myPeerID;
+
+@property (nonatomic, strong) UIButton *browserButton;
+@property (nonatomic, strong) UITextField *chatBox;
+@property (nonatomic, strong) UITextView *textBox;
 
 @end
